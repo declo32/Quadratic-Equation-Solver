e7ba0ce05555c7b6ea5da7bcabba321f239cd89c
Declan O'Neill <declanone32@gmail.com>
2018-11-07 11:32:39 -0500

    Change comments

--- src/QuadraticEquationSolverTester.java
+++ src/QuadraticEquationSolverTester.java
@@ -3,7 +3,7 @@ public class QuadraticEquationSolverTester {
 		QuadraticEquationSolver[] quadraticEquationSolvers = {
 				new QuadraticEquationSolver(1, 0, 0, 0),  // x^2 = 0           (one real solution)
 				new QuadraticEquationSolver(1, 2, 3, 4),  // x^2 + 2x + 3 = 4  (two real solutions)
-				new QuadraticEquationSolver(1, 0, 1, 0),  // x^2 + 1 = 0       (one imaginary solution) FAILS HERE
+				new QuadraticEquationSolver(1, 0, 1, 0),  // x^2 + 1 = 0       (two imaginary solutions) FAILS HERE
 		};
 
 		for (QuadraticEquationSolver quadraticEquationSolver : quadraticEquationSolvers) {
